1b25d506ff26881566d3c4903738dd8bf9e591d4
Kushal patil <kushalpatil@mac.mynetworksettings.com>
2026-03-13 22:49:26 -0400

    Adding files

---
+++ src/components/About.vue
@@ -0,0 +1,54 @@
+<script setup>
+import AboutItem from "./AboutItem.vue";
+</script>
+
+<template>
+  <AboutItem>
+    <template #icon>
+      <DocumentationIcon />
+    </template>
+    <template #heading>Who Are We?</template>
+    We are a nonprofit organization dedicated to offering free computer science education through classes held at middle schools and libraries. With locations in Stowe and Sudbury, we aim to make computer science accessible to all. Our courses provide an opportunity for students to explore their interest in technology and build valuable skills. 
+    <a class="text-blue-500" href="#/team" type="link"><u> our team</u>.</a>
+  </AboutItem>
+
+  <AboutItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Goals</template>
+
+    At the heart of our mission is a commitment to helping students develop a curiosity for computer science and equip them with the essential skills needed to thrive in a tech-driven world. We are passionate about fostering a generation of individuals who are confident and capable in navigating the digital landscape, opening doors for personal and professional growth.
+  </AboutItem>
+  <AboutItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Programs & Classes</template>
+
+    Our classes are designed for anyone eager to learn programming, from beginners to those looking to deepen their skills. Parents who value teaching their children early technological and problem-solving skills will find our programs to be a worthwhile investment. We offer a range of comprehensive courses, including Unity, Raspberry Pi, Python, and more! Find out more here.
+    <a class="text-blue-500" href="#/enroll" type="link"><u>here</u>.</a>
+  </AboutItem>
+  <AboutItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Future</template>
+
+    As we continue expanding our reach, we aim to offer even more diverse and accessible computer science programs. Our goal is to broaden our curriculum, offering a variety of programming concepts to suit the varied interests and backgrounds of students. We are committed to making our resources available to as many students as possible.
+  </AboutItem>
+  <AboutItem>
+    <template #icon>
+      <ToolingIcon />
+    </template>
+    <template #heading>Donations</template>
+
+    Support our mission 
+    <a
+      class="text-blue-500"
+      href="https://hcb.hackclub.com/donations/start/programmatic"
+      type="link"
+      ><u>here</u></a
+    >, to provide free computer science education to all. Any donation, no matter the size, helps keep our programs accessible. Every dollar directly supports the resources, tools, and materials that help our students succeed.
+  </AboutItem>
+</template>
---
+++ src/views/OurTeamView.vue
@@ -0,0 +1,138 @@
+<script setup>
+import BioCard from "../components/BioCard.vue";
+</script>
+
+<template>
+  <main class="flex OurTeam">
+    <section class="fixed OurteamTxt">
+      <h2 class="font-extrabold text-gray-200 text-6xl mt-32 ourTeamTitle">
+        Meet the Team:
+      </h2>
+      <h3
+        class="text-lg font-bold py-10 ourTeam-desc text-gray-300 OurTeamDesc"
+      >
+
+      This is the team of founders, developers, and teachers behind Programmatic. Our team is the driving force behind the continued development and improvement of Programmatic. With a blend of visionary founders, skilled developers, and creative educators, we are committed to providing excellent teaching materials, refining Programmatic, and creating impactful experiences for our students.
+      </h3>
+    </section>
+    <section class="line"></section>
+    <section class="flex flex-col gap-20 mt-36 mb-10 cards">
+      <BioCard
+        name="Ofir"
+        pos="Founder, Front-End Developer, Teacher"
+        desc="Hi, I'm Ofir, the founder of Programmatic. With over 5 years of coding experience, I specialize in Python and have recently started exploring Unity and Dart to create practical applications. I’m excited to help guide you through your programming journey!"
+        langs="Python, Dart, C#, JavaScript"
+        discord="@.ofish"
+        imgurl="discord.png"
+      />
+      <BioCard
+        name="Zain"
+        desc="Hi, I’m Zain! I’m excited to teach you about programming. I have 3 years of experience working with both Python and Raspberry Pi, and I’m passionate about helping others learn to build their own tech projects."
+        langs="Python, Raspberry Pi, Circuits"
+        discord="@zkzain06"
+        pos="Founder, Front-End Developer, Teacher"
+        imgurl="discord.png"
+      />
+
+      <BioCard
+        name="Adam"
+        desc="Hello, I’m Adam. With about 4 years of experience in Unity, I specialize in game development. I’m here to help you understand how to bring your creative ideas to life through coding."
+        langs="Python, C#"
+        discord="@Adamimoka"
+        pos="Founder, Graphic Designer, Front-End Developer"
+        imgurl="discord.png"
+      />
+
+      <BioCard
+        name="Ethan"
+        desc="Hi, I’m Ethan! I work as a full-stack developer and maintain the Programmatic Discord bot. I also develop websites and applications to help your learning experience."
+        langs="Python, C++, Java, JavaScript"
+        discord="@ethqnol"
+        pos="Full-Stack Developer, Dev of the Programmatic Discord Bot"
+        imgurl="discord.png"
+      />
+    </section>
+  </main>
+</template>
+
+<style scoped>
+body {
+  padding: 0;
+  margin: 0;
+}
+
+.wrapper {
+  display: flex;
+}
+
+nav {
+  flex: 0 0 50vw;
+  background: gray;
+}
+
+.nav-wrapper {
+  position: fixed;
+  top: 0;
+  left: 0;
+}
+
+section {
+  flex: 1 1;
+}
+
+/* media 1350 */
+
+@media (max-width: 1350px) {
+  .line {
+    display: none;
+  }
+
+  .OurteamTxt {
+    position: inherit;
+  }
+
+  .OurTeam {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .cards {
+    margin-top: 1rem;
+  }
+
+  .OurTeamDesc {
+    width: 800px !important;
+    max-width: 800px !important;
+    text-align: center;
+  }
+
+  .ourTeamTitle {
+    text-align: center;
+  }
+
+  /* media 1000px */
+  @media (max-width: 1000px) {
+    .OurTeamDesc {
+      width: 600px !important;
+      max-width: 600px !important;
+    }
+  }
+
+  /* media 650px */
+  @media (max-width: 650px) {
+    .OurTeamDesc {
+      width: 400px !important;
+      max-width: 400px !important;
+    }
+  }
+
+  /* media 450px */
+  @media (max-width: 450px) {
+    .OurTeamDesc {
+      width: 370px !important;
+      max-width: 370px !important;
+    }
+  }
+}
+</style>
